c6c49b38c5bc6912d0cc667c4307be4568098386
hvelarde <hector.velarde@gmail.com>
2012-06-18 16:59:38 -0300

    fixed development buildout configurations

--- setup.py
+++ setup.py
@@ -2,27 +2,36 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.composition',
       version=version,
       description="Composable page",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_description,
       classifiers=[
         "Development Status :: 3 - Alpha",
-        "Framework :: Plone :: 4.1",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
         "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone composition javascript',
       author='Carlos de la Guardia',
       author_email='cguardia@yahoo.com',
-      url='https://github.com/cguardia/collective.composition',
-      license='GPL version 2',
+      url='https://github.com/collective/collective.composition',
+      license='GPLv2',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['collective'],
@@ -36,7 +45,7 @@ setup(name='collective.composition',
         'plone.app.tiles',
         'plone.tiles',
         'zope.component>=3.8.0',
-        'plone.app.dexterity>=1.1',
+        'plone.app.dexterity>=1.2.1',
         'collective.js.jqueryui',
         'pyquery'
         ],
