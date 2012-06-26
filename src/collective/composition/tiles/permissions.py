f3f21b187e960bfbbc224aece91745e5f5ec04ae
Franco Pellegrini <frapell@gmail.com>
2012-06-26 19:08:42 -0300

    Implemented a permission interface to allow setting groups as allowed to edit tiles content. Override the edit-tile view so we can check for permissions, and raise Unauthorized in case the user doesn't belong to the proper group. Also added code for the base tiles class to check for permissions. fixes gh-36

---
+++ src/collective/composition/tiles/permissions.py
@@ -0,0 +1,55 @@
+
+from persistent.dict import PersistentDict
+
+from zope.annotation.interfaces import IAnnotations
+
+from zope.interface import implements
+from zope.interface import Interface
+
+
+ANNOTATIONS_KEY_PREFIX = u'plone.tiles.permission'
+
+
+class ITilesPermissions(Interface):
+
+    def get_allowed_edit():
+        """
+        Get the list of groups that are allowed to add content to the tile
+        """
+
+    def set_allowed_edit(group_ids):
+        """
+        Set the list of groups that are allowed to add content to the tile
+        """
+
+
+class TilesPermissions(object):
+    """
+    An adapter that will provide store permissions for a tile
+    """
+
+    implements(ITilesPermissions)
+
+    def __init__(self, context, request, tile):
+        self.context = context
+        self.request = request
+        self.tile = tile
+        self.annotations = IAnnotations(self.context)
+        self.key = "%s.%s" % (ANNOTATIONS_KEY_PREFIX, tile.id,)
+
+    def get_allowed_edit(self):
+        permissions = dict(self.annotations.get(self.key, {}))
+
+        return permissions.get('edit', ())
+
+    def set_allowed_edit(self, group_ids):
+        permissions = dict(self.annotations.get(self.key, {}))
+
+        if isinstance(group_ids, list):
+            group_ids = tuple(group_ids)
+        elif isinstance(group_ids, basestring):
+            group_ids = (group_ids,)
+
+        permissions['edit'] = group_ids
+
+        self.annotations[self.key] = PersistentDict(permissions)
