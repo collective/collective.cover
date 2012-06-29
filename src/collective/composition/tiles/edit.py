ed7ac7983c42c103fea5923bcc9b55a564ec1d0f
Franco Pellegrini <frapell@gmail.com>
2012-06-29 10:14:14 -0300

    When using the edit-tile form, do not overwrite fields that are not included in the form and have already content in the tile. Fixes gh-42

--- src/collective/composition/tiles/edit.py
+++ src/collective/composition/tiles/edit.py
@@ -1,12 +1,27 @@
 
+from z3c.form import button
+
 from AccessControl import Unauthorized
 
+from zope.lifecycleevent import ObjectModifiedEvent
+from zope.event import notify
+
 from zope.publisher.interfaces.browser import IBrowserView
+
+from zope.traversing.browser.absoluteurl import absoluteURL
 
 from plone.app.tiles.browser.edit import DefaultEditForm
 from plone.app.tiles.browser.edit import DefaultEditView
 
 from plone.app.tiles.browser.traversal import EditTile
+
+from plone.tiles.interfaces import ITileDataManager
+
+from plone.app.tiles.utils import appendJSONData
+
+from Products.statusmessages.interfaces import IStatusMessage
+
+from collective.composition import _
 
 
 class ICompositionTileEditView(IBrowserView):
@@ -31,6 +46,52 @@ class CustomEditForm(DefaultEditForm):
 
         if not tile.isAllowedToEdit():
             raise Unauthorized("You are not allowed to add this kind of tile")
+
+    @button.buttonAndHandler(_('Save'), name='save')
+    def handleSave(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+
+        typeName = self.tileType.__name__
+
+        # Traverse to a new tile in the context, with no data
+        tile = self.context.restrictedTraverse('@@%s/%s' % (typeName, self.tileId,))
+
+        dataManager = ITileDataManager(tile)
+
+        # We need to check first for existing content in order to not loose
+        # fields that weren't sent with the form.
+        old_data = dataManager.get()
+        for item in data:
+            old_data[item] = data[item]
+
+        dataManager.set(old_data)
+
+        # Look up the URL - we need to do this after we've set the data to
+        # correctly account for transient tiles
+        tileURL = absoluteURL(tile, self.request)
+        contextURL = absoluteURL(tile.context, self.request)
+        tileRelativeURL = tileURL
+
+        if tileURL.startswith(contextURL):
+            tileRelativeURL = '.' + tileURL[len(contextURL):]
+
+        notify(ObjectModifiedEvent(tile))
+
+        # Get the tile URL, possibly with encoded data
+        IStatusMessage(self.request).addStatusMessage(_(u"Tile saved",), type=u'info')
+
+        self.request.response.redirect(tileURL)
+
+    @button.buttonAndHandler(_(u'Cancel'), name='cancel')
+    def handleCancel(self, action):
+        tileDataJson = {}
+        tileDataJson['action'] = "cancel"
+        url = self.request.getURL()
+        url = appendJSONData(url, 'tiledata', tileDataJson)
+        self.request.response.redirect(url)
 
 
 class CustomTileEdit(DefaultEditView):
