b9fa8230edcb886f57a79aed4687d123b67cc51d
Gonzalo Almeida <flecox@gmail.com>
2012-07-18 19:14:16 -0300

    adding tileupdate view

--- src/collective/cover/content.py
+++ src/collective/cover/content.py
@@ -219,6 +219,23 @@ class UpdateTileContent(grok.View):
         #      find out if this affects us in any way.
         return tile_instance()
 
+
+class UpdateTile(grok.View):
+    grok.context(ICover)
+    grok.require('cmf.ModifyPortalContent')
+
+    def render(self):
+        pc = getToolByName(self.context, 'portal_catalog')
+
+        tile_type = self.request.form.get('tile-type')
+        tile_id = self.request.form.get('tile-id')
+
+        if tile_type and tile_id:
+            tile = self.context.restrictedTraverse(tile_type)
+            tile_instance = tile[tile_id]
+        return tile_instance()
+
+
 class UpdateListTileContent(grok.View):
     grok.context(ICover)
     grok.require('cmf.ModifyPortalContent')
