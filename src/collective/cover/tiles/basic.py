b8d620da30a3ab1c67fa48b94ebd000dea4d82c3
Juan Pablo Giménez <jpg@rcom.com.ar>
2012-07-20 09:22:29 -0300

    - basic tile, tags and date support

--- src/collective/cover/tiles/basic.py
+++ src/collective/cover/tiles/basic.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from zope import schema
 from zope.interface import implements
 
 from plone.namedfile.field import NamedBlobImage as NamedImage
 from plone.namedfile.file import NamedBlobImage as NamedImageFile
 from plone.tiles.interfaces import ITileDataManager
+try:
+    from z3c.form.browser.textlines import TextLinesFieldWidget
+except ImportError:
+    from plone.z3cform.textlines.textlines import TextLinesFieldWidget
+from collective.z3cform.datetimewidget.widget_datetime import DatetimeWidget
+from plone.autoform import directives as form
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -31,6 +38,22 @@ class IBasicTileData(IPersistentCoverTile):
         required=False,
         )
 
+    date = schema.Datetime(
+        title=_(u'Date'),
+        required=False,
+        )
+
+    subjects = schema.Tuple(
+        title=_(u'label_categories', default=u'Categories'),
+        description = _(u'help_categories', 
+                        default=u'Also known as keywords, tags or labels, these help you categorize your content.'),
+        required=False,
+        value_type = schema.TextLine(),
+        missing_value = (),
+        )
+
+    form.widget(tags = TextLinesFieldWidget)
+
     def get_title():
         """
         A method to return the title stored in the tile
@@ -44,6 +67,16 @@ class IBasicTileData(IPersistentCoverTile):
     def get_image():
         """
         A method to return the image stored in the tile
+        """
+
+    def get_date():
+        """
+        A method to return the date stored in the tile
+        """
+
+    def get_subjects():
+        """
+        A method to return the subjects stored in the tile
         """
 
     def populate_with_object(obj):
@@ -75,10 +108,32 @@ class BasicTile(PersistentCoverTile):
     def get_image(self):
         return self.data['image']
 
+    def get_date(self):
+        """
+        A method to return the date stored in the tile
+        """
+        if self.data['date'] == None:
+            return ''
+        formatter = self.request.locale.dates.getFormatter("dateTime", "short")
+        datetime_value = self.data['date']
+        if datetime_value.year > 1900:
+            return formatter.format(datetime_value)
+        # due to fantastic datetime.strftime we need this hack
+        # for now ctime is default
+        return datetime_value.ctime()
+
+    def get_subjects(self):
+        """
+        A method to return the subjects stored in the tile
+        """
+        return self.data['subjects']
+
     def is_empty(self):
         return not(self.data['title'] or \
                    self.data['description'] or \
-                   self.data['image'])
+                   self.data['image'] or \
+                   self.data['date'] or \
+                   self.data['subjects'])
 
     def populate_with_object(self, obj):
         super(BasicTile, self).populate_with_object(obj)
