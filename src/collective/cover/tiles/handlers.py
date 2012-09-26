b0e21b53fbccb0ba08954b815a5716918c47b94c
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-09-26 08:40:53 -0300

    Fix notify event, apply notified for IPersistentCoverTile interface

--- src/collective/cover/tiles/handlers.py
+++ src/collective/cover/tiles/handlers.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
 from zope.component import adapter
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
-from plone.tiles.interfaces import ITile
+from collective.cover.tiles.base import IPersistentCoverTile
 
 
-@adapter(ITile, IObjectModifiedEvent)
+@adapter(IPersistentCoverTile, IObjectModifiedEvent)
 def notifyModified(tile, event):
     # Make sure the page's modified date gets updated, necessary in cache purge
     # cases by eg
