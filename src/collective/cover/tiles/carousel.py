a837eb77b5d77a1be8e0489d365e9f01999d0158
hvelarde <hector.velarde@gmail.com>
2012-09-26 16:54:41 -0300

    carousel tile is not editable; pyflakes

--- src/collective/cover/tiles/carousel.py
+++ src/collective/cover/tiles/carousel.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from zope.interface import Interface
-from zope import schema
-from zope.component import queryUtility
-from plone.app.textfield.interfaces import ITransformer
-
-from plone.uuid.interfaces import IUUID
-from plone.app.uuid.utils import uuidToObject
-from plone.tiles.interfaces import ITileDataManager
-from plone.namedfile.field import NamedImage
-
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-from collective.cover import _
-from collective.cover.tiles.base import IPersistentCoverTile
 from collective.cover.tiles.list import ListTile, IListTile
 
 
@@ -21,6 +9,8 @@ class ICarouselTile(IListTile):
     """
     """
 
+
 class CarouselTile(ListTile):
     index = ViewPageTemplateFile("templates/carousel.pt")
     is_configurable = False
+    is_editable = False
