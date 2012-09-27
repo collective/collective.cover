75d4eeb1a88bedfa03f39f7acb52ff567c3af1a8
Gonzalo Almeida <flecox@gmail.com>
2012-09-27 15:06:54 -0300

    fixing element amount to show

--- src/collective/cover/tiles/list.py
+++ src/collective/cover/tiles/list.py
@@ -8,7 +8,9 @@ from plone.app.textfield.interfaces import ITransformer
 from plone.uuid.interfaces import IUUID
 from plone.app.uuid.utils import uuidToObject
 from plone.tiles.interfaces import ITileDataManager
+from plone.tiles.interfaces import ITileType
 from plone.namedfile.field import NamedImage
+from zope.schema import getFieldsInOrder
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -19,8 +21,8 @@ from collective.cover.tiles.base import PersistentCoverTile
 
 class IListTile(IPersistentCoverTile):
 
-    uuids = schema.List(title=u'Item uuids',
-        value_type=schema.TextLine(), readonly=True)
+    uuids = schema.List(title=_(u'Elements'),
+        value_type=schema.TextLine())
 
     title = schema.TextLine(
         title=_(u'Title'),
@@ -64,10 +66,12 @@ class ListTile(PersistentCoverTile):
 
     index = ViewPageTemplateFile("templates/list.pt")
 
-    is_configurable = False
+    is_configurable = True
+    is_editable = False
     limit = 4
 
     def results(self):
+        self.set_limit()
         start = 0
         uuids = self.data.get('uuids', None)
         result = []
@@ -79,11 +83,17 @@ class ListTile(PersistentCoverTile):
                     result.append(obj)
                 else:
                     self.remove_item(uid)
-
-        return result
+        return result[:self.limit]
+    
+    def set_limit(self):
+        for field in self.get_configured_fields():
+            if field and 'id' in field.keys() and 'size' in field.keys() \
+            and field['id'] == 'uuids':
+                self.limit = int(field['size'])
 
     def populate_with_object(self, obj):
         super(ListTile, self).populate_with_object(obj)
+        self.set_limit()
         uuid = IUUID(obj, None)
         data_mgr = ITileDataManager(self)
 
@@ -102,6 +112,7 @@ class ListTile(PersistentCoverTile):
 
     def replace_with_objects(self, objs):
         super(ListTile, self).replace_with_objects(objs)
+        self.set_limit()
         data_mgr = ITileDataManager(self)
         old_data = data_mgr.get()
         if type(objs) == list:
@@ -130,3 +141,37 @@ class ListTile(PersistentCoverTile):
 
     def accepted_ct(self):
         return None
+
+    def get_configured_fields(self):
+        # Override this method, since we are not storing anything
+        # in the fields, we just use them for configuration
+        tileType = queryUtility(ITileType, name=self.__name__)
+        conf = self.get_tile_configuration()
+
+        fields = getFieldsInOrder(tileType.schema)
+
+        results = []
+        for name, obj in fields:
+            field = {'id': name,
+                     'title': obj.title}
+            if name in conf:
+                field_conf = conf[name]
+                if ('visibility' in field_conf and field_conf['visibility'] == u'off'):
+                    # If the field was configured to be invisible, then just
+                    # ignore it
+                    continue
+
+                if 'htmltag' in field_conf:
+                    # If this field has the capability to change its html tag
+                    # render, save it here
+                    field['htmltag'] = field_conf['htmltag']
+
+                if 'imgsize' in field_conf:
+                    field['scale'] = field_conf['imgsize']
+
+                if 'size' in field_conf:
+                    field['size'] = field_conf['size']
+
+            results.append(field)
+
+        return results
