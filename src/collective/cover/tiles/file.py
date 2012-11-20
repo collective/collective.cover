d8370ba6de2cc5d002d20967ebc286522a727759
hvelarde <hector.velarde@gmail.com>
2012-11-20 17:23:03 -0200

    refactor of file tile; increase test code coverage

--- src/collective/cover/tiles/file.py
+++ src/collective/cover/tiles/file.py
@@ -5,10 +5,9 @@ from zope.interface import implements
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-from plone.namedfile.field import NamedBlobImage as NamedImage
+from plone.app.uuid.utils import uuidToObject
 from plone.tiles.interfaces import ITileDataManager
 from plone.uuid.interfaces import IUUID
-from plone.app.uuid.utils import uuidToObject
 
 from collective.cover import _
 from collective.cover.tiles.base import IPersistentCoverTile
@@ -27,6 +26,7 @@ HTML = """
 """
 
 
+# XXX: refactor this to make it easier to test
 def get_download_html(url, portal_url, icon, mime_type, size):
     if size < 1024:
         size_str = '%s bytes' % size
@@ -47,11 +47,6 @@ class IFileTile(IPersistentCoverTile):
 
     description = schema.Text(
         title=_(u'Description'),
-        required=False,
-    )
-
-    image = NamedImage(
-        title=_(u'Image'),
         required=False,
     )
 
@@ -78,13 +73,6 @@ class FileTile(PersistentCoverTile):
     is_editable = True
     is_droppable = True
 
-    # XXX: initialize tile? how?
-    #def __init__(self, context, request):
-    #    super(PersistentCoverTile, self).__init__(context, request)
-
-        #data_mgr = ITileDataManager(self)
-        #data_mgr.set({'title': '', 'description': '', 'image': '', 'download': '', 'uuid': ''})
-
     def get_title(self):
         """ Returns the title stored in the tile.
         """
@@ -95,11 +83,7 @@ class FileTile(PersistentCoverTile):
         """
         return self.data['description']
 
-    def get_image(self):
-        """ Returns the image stored in the tile.
-        """
-        return self.data['image']
-
+    # XXX: refactor this to make it easier to test
     def download_widget(self):
         """ Returns a download link for the file associated with the tile.
         """
@@ -112,33 +96,25 @@ class FileTile(PersistentCoverTile):
             size = obj.get_size()
             return get_download_html(url, portal_url, icon, mime, size)
 
-    def get_date(self):
-        """ Returns the date of the file associated with the tile.
-        """
-        # TODO: support selection of which date we want to display
-        obj = uuidToObject(self.data['uuid'])
-        if obj:
-            return obj.Date()
-
     def is_empty(self):
-        return not(self.data.get('title') or
-                   self.data.get('description') or
-                   self.data.get('image') or
-                   self.data.get('uuid'))
+        return not(self.data.get('title', None) or \
+                   self.data.get('description', None) or \
+                   self.data.get('uuid', None))
 
     def populate_with_object(self, obj):
         super(FileTile, self).populate_with_object(obj)  # check permissions
 
-        title = obj.Title() if hasattr(obj, 'Title') else None
-        description = obj.Description() if hasattr(obj, 'Description') else None
-        uuid = IUUID(obj, None)
+        if obj.portal_type in self.accepted_ct():
+            title = obj.Title()
+            description = obj.Description()
+            uuid = IUUID(obj)
 
-        data_mgr = ITileDataManager(self)
-        data_mgr.set({'title': title,
-                      'description': description,
-                      'download': True,
-                      'uuid': uuid,
-                      })
+            data_mgr = ITileDataManager(self)
+            data_mgr.set({'title': title,
+                          'description': description,
+                          'download': True,
+                          'uuid': uuid,
+                          })
 
     def accepted_ct(self):
         """ Return a list of content types accepted by the tile.
