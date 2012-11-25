16e52eda6236e0a4772f3cf62795f86bd93a79df
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-11-25 20:49:52 -0200

    Initialize solution to issue #85

--- src/collective/cover/interfaces.py
+++ src/collective/cover/interfaces.py
@@ -6,3 +6,16 @@ from zope.interface import Interface
 class ICoverLayer(Interface):
     """ A layer specific for this add-on product.
     """
+
+
+class IJSONSearch(Interface):
+    """ Returns a list of search results in JSON """
+
+    def __init__(self, context):
+        """  Constructor """
+
+    def getBreadcrumbs(self):
+        """Get breadcrumbs"""
+
+    def getSearchResults(self, filter_portal_types, rooted, document_base_url, searchtext):
+        """ Returns the actual search results """
