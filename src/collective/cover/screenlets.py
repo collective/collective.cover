a324523dfe31c121a20a429f8004a2764638b5b8
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-11-25 21:01:24 -0200

    Added methods to content-tree tab

--- src/collective/cover/screenlets.py
+++ src/collective/cover/screenlets.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from zope.component import getUtility  # , getMultiAdapter
+from zope.component import getMultiAdapter, getUtility, queryUtility
+
 from zope.interface import Interface
 from zope.schema.vocabulary import SimpleTerm
 
@@ -13,8 +14,30 @@ from plone.registry.interfaces import IRegistry
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.browser.navtree import SitemapNavtreeStrategy
+from Products.Five.browser import BrowserView
 
 from collective.cover.controlpanel import ICoverSettings
+
+from collective.cover.interfaces import IJSONSearch
+from Products.CMFCore.interfaces._content import IFolderish
+from plone.app.vocabularies.types import ReallyUserFriendlyTypesVocabulary
+from zope.schema.interfaces import IVocabularyFactory
+from zope.interface import implements
+
+try:
+    import json
+    json = json  # Pyflakes
+except ImportError:
+    import simplejson as json
+
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from plone.app.layout.navigation.root import getNavigationRoot
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from Acquisition import aq_inner, aq_parent
+
+
+VOCAB_ID = u'plone.app.vocabularies.ReallyUserFriendlyTypes'
+
 
 grok.templatedir("screenlets_templates")
 
@@ -124,3 +147,112 @@ class ContentSearch(grok.View):
         self.portal_path = portal_tool.getPortalPath()
         value = brain.getPath()[len(self.portal_path):]
         return SimpleTerm(value, token=brain.getPath(), title=brain.Title)
+
+
+class SearchItemsBrowserView(BrowserView):
+    """ Returns a folderish like listing in JSON """
+
+    def __init__(self, context, request, **kwargs):
+        """ Contructor """
+        self.context = context
+        self.request = request
+        self.catalog = getToolByName(self.context, 'portal_catalog')
+        self.plone_view = getMultiAdapter((self.context, self.request), name=u'plone')
+        self.getIcon = self.plone_view.getIcon
+        self.registry = getUtility(IRegistry)
+        self.settings = self.registry.forInterface(ICoverSettings)
+
+        # check if object is a folderish object, if not, get it's parent.
+        if not IFolderish.providedBy(self.context):
+            self.obj = aq_parent(self.context)
+        else:
+            self.obj = aq_inner(self.context)
+
+    def _getCurrentValues(self):
+        """Return enabled portal types"""
+        vocab = queryUtility(IVocabularyFactory, name=VOCAB_ID)(self.context)
+        portal_types = getToolByName(self.context, 'portal_types', None)
+        result = []
+        # the vocabulary returns the values sorted by their translated title
+        for term in vocab._terms:
+            value = unicode(portal_types[term.value].Title())  # portal_type
+            title = term.title  # already translated title
+            result.append((value, title))
+
+        return result
+
+    def getBreadcrumbs(self, path=None):
+        """ Get breadcrumbs """
+        result = []
+        root_url = getNavigationRoot(self.obj)
+        root = aq_inner(self.obj.restrictedTraverse(root_url))
+        root_url = root.absolute_url()
+
+        if path is not None:
+            root_abs_url = root.absolute_url()
+            path = path.replace(root_abs_url, '', 1)
+            path = path.strip('/')
+            root = aq_inner(root.restrictedTraverse(path))
+
+        relative = aq_inner(self.obj).getPhysicalPath()[len(root.getPhysicalPath()):]
+        if path is None:
+            # Add siteroot
+            result.append({'title': root.title_or_id(), 'url': '/'.join(root.getPhysicalPath())})
+
+        for i in range(len(relative)):
+            now = relative[:i + 1]
+            obj = aq_inner(root.restrictedTraverse(now))
+
+            if IFolderish.providedBy(obj):
+                if not now[-1] == 'talkback':
+                    result.append({'title': obj.title_or_id(), 'url': root_url + '/' + '/'.join(now)})
+        return result
+
+    def jsonByType(self, rooted, document_base_url, searchtext):
+        """ Returns the actual listing """
+        catalog_results = []
+        results = {}
+
+        object = self.obj
+        portal_catalog = getToolByName(object, 'portal_catalog')
+        normalizer = getUtility(IIDNormalizer)
+
+        if 'filter_portal_types' in self.request.keys():
+            self.filter_portal_types = self.request['filter_portal_types']
+        else:
+            self.filter_portal_types = [i[0] for i in self._getCurrentValues()]
+
+        if INavigationRoot.providedBy(object) or (rooted == "True" and document_base_url[:-1] == object.absolute_url()):
+            results['parent_url'] = ''
+        else:
+            results['parent_url'] = aq_parent(object).absolute_url()
+        if rooted == "True":
+            results['path'] = self.getBreadcrumbs(results['parent_url'])
+        else:
+            # get all items from siteroot to context (title and url)
+            results['path'] = self.getBreadcrumbs()
+        # get all portal types and get information from brains
+        path = '/'.join(object.getPhysicalPath())
+
+        catalog_query = {'sort_on': 'getObjPositionInParent'}
+        catalog_query['portal_type'] = self.filter_portal_types
+        catalog_query['path'] = {'query': path, 'depth': 1}
+        if searchtext:
+            catalog_query = {'SearchableText': '%s*' % searchtext}
+
+        for brain in portal_catalog(**catalog_query):
+            catalog_results.append({
+                'id': brain.getId,
+                'uid': brain.UID or None,  # Maybe Missing.Value
+                'url': brain.getURL(),
+                'portal_type': brain.portal_type,
+                'normalized_type': normalizer.normalize(brain.portal_type),
+                'classicon': 'contenttype-%s' %(normalizer.normalize(brain.portal_type)),
+                'title': brain.Title == "" and brain.id or brain.Title,
+                'icon': self.getIcon(brain).html_tag() or '',
+                'is_folderish': brain.is_folderish
+                })
+        # add catalog_ressults
+        results['items'] = catalog_results
+        # return results in JSON format
+        return json.dumps(results)
