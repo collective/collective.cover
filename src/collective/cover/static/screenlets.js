b075c642a8a1b223ba5472245332b3660e188c0c
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-11-28 16:02:11 -0200

    Fix search objects

--- src/collective/cover/static/screenlets.js
+++ src/collective/cover/static/screenlets.js
@@ -155,8 +155,6 @@ var coveractions = {
         if (this.current_path.indexOf('/compose') > 0){
             this.getFolderContents(this.call_context,'@@jsonbytype')
 
-            // LiveSearch
-            this.liveSearch();
         };
 
     },
@@ -322,24 +320,27 @@ var coveractions = {
         jQuery(selector).keyup(function(){
             // Retrieve the input field text and reset the count to zero
             var filter = $(this).val(), count = 0;
-        
+
             // Loop through the items list
             jQuery(selectorlist).each(function(){
-        
                 // If the list item does not contain the text phrase fade it out
                 if (jQuery(this).text().search(new RegExp(filter, "i")) < 0) {
                     jQuery(this).fadeOut();
-        
                 // Show the list item if the phrase matches and increase the count by 1
                 } else {
                     jQuery(this).show();
                     count++;
                 }
+
             });
         
             // Update the count
-            var numberItems = count;
-            jQuery(selectoroutput).text(" "+ count + " Results");
+            if (filter != ""){
+                var numberItems = count;
+                jQuery(selectoroutput).text(" "+ count + " Results");
+            } else {
+                jQuery(selectoroutput).text("")
+            }
         });
     },
 
