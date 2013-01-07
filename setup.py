cad114166d1d64a44cb316ca8bbc2cc424687f2b
hvelarde <hector.velarde@gmail.com>
2013-01-07 17:38:52 -0200

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0a1'
+version = '1.0a2.dev0'
 long_description = open("README.txt").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
