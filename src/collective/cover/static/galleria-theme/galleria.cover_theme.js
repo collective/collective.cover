96e4bc317600c10927d7daa65789d81ef7512732
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-02-20 10:17:20 -0300

    Galleria integration and custom cover theme for it...

---
+++ src/collective/cover/static/galleria-theme/galleria.cover_theme.js
@@ -0,0 +1,79 @@
+/**
+ * collective.cover - Galleria Theme 2013-02-18
+ * http://galleria.io
+ *
+ * Licensed under the MIT license
+ * https://raw.github.com/aino/galleria/master/LICENSE
+ *
+ */
+
+(function($) {
+
+/*global jQuery, Galleria */
+
+Galleria.addTheme({
+    name: 'cover_theme',
+    author: 'Simples',
+    css: 'galleria.cover_theme.css',
+    defaults: {
+        transition: 'slide',
+        transition_speed: 500,
+        thumbCrop:  'height',
+        thumbnails: 'empty',
+        carousel: false,
+        image_crop: false,
+        autoplay: true,
+        _toggleInfo: false
+    },
+    init: function(options) {
+
+        Galleria.requires(1.28, 'This version of Classic theme requires Galleria 1.2.8 or later');
+
+        // cache some stuff
+        var touch = Galleria.TOUCH,
+            click = touch ? 'touchstart' : 'click';
+
+        // show loader & counter with opacity
+        this.$('loader,counter').show().css('opacity', 0.4);
+
+        // some stuff for non-touch browsers
+        if (! touch ) {
+            this.addIdleState( this.get('image-nav-left'), { left:-50 });
+            this.addIdleState( this.get('image-nav-right'), { right:-50 });
+            this.addIdleState( this.get('counter'), { opacity:0 });
+        }
+
+        // bind some stuff
+        this.bind('thumbnail', function(e) {
+
+            if (! touch ) {
+                // fade thumbnails
+                $(e.thumbTarget).css('opacity', 0.6).parent().hover(function() {
+                    $(this).not('.active').children().stop().fadeTo(100, 1);
+                }, function() {
+                    $(this).not('.active').children().stop().fadeTo(400, 0.6);
+                });
+
+                if ( e.index === this.getIndex() ) {
+                    $(e.thumbTarget).css('opacity',1);
+                }
+            } else {
+                $(e.thumbTarget).css('opacity', this.getIndex() ? 1 : 0.6);
+            }
+        });
+
+        this.bind('loadstart', function(e) {
+            if (!e.cached) {
+                this.$('loader').show().fadeTo(200, 0.4);
+            }
+
+            $(e.thumbTarget).css('opacity',1).parent().siblings().children().css('opacity', 0.6);
+        });
+
+        this.bind('loadfinish', function(e) {
+            this.$('loader').fadeOut(200);
+        });
+    }
+});
+
+}(jQuery));
