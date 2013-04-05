fe9253f4c696df96c046389a4c1dfdb3b9b8f04b
hvelarde <hector.velarde@gmail.com>
2013-04-05 15:34:10 -0300

    skip test_screenlet_search_tab if running under Plone 4.3 (refs. #155)

--- src/collective/cover/tests/test_acceptance.py
+++ src/collective/cover/tests/test_acceptance.py
@@ -6,6 +6,9 @@ from plone.testing import layered
 
 from collective.cover.testing import FUNCTIONAL_TESTING
 
+import pkg_resources
+PLONE_VERSION = pkg_resources.require("Plone")[0].version
+
 
 def test_suite():
     suite = unittest.TestSuite()
@@ -20,8 +23,6 @@ def test_suite():
                 layer=FUNCTIONAL_TESTING),
         layered(robotsuite.RobotTestSuite("test_contenttree_tab_path.txt"),
                 layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_screenlet_search_tab.txt"),
-                layer=FUNCTIONAL_TESTING),
         layered(robotsuite.RobotTestSuite("test_collection_tile.txt"),
                 layer=FUNCTIONAL_TESTING),
         layered(robotsuite.RobotTestSuite("test_embed_tile.txt"),
@@ -35,4 +36,11 @@ def test_suite():
         layered(robotsuite.RobotTestSuite("test_locked_cover.txt"),
                 layer=FUNCTIONAL_TESTING),
     ])
+    # FIXME: test randomly failing under Plone 4.3.x
+    #        see https://github.com/collective/collective.cover/issues/155
+    if '4.3' not in PLONE_VERSION:
+        suite.addTests([
+            layered(robotsuite.RobotTestSuite("test_screenlet_search_tab.txt"),
+                    layer=FUNCTIONAL_TESTING),
+        ])
     return suite
