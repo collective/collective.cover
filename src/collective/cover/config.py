a99855c442f35403c4c26f8c9d25bfd5fd252ef9
Érico Andrei <erico@simplesconsultoria.com.br>
2013-04-05 19:29:30 -0300

    Add PFG to searchable content types

--- src/collective/cover/config.py
+++ src/collective/cover/config.py
@@ -5,6 +5,7 @@ PROJECTNAME = 'collective.cover'
 DEFAULT_SEARCHABLE_CONTENT_TYPES = ['Collection',
                                     'Document',
                                     'File',
+                                    'Form Folder',
                                     'Image',
                                     'Link',
                                     'News Item']
