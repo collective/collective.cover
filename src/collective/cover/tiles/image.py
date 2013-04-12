36a970c370ce78367121de4acee9c5bc816d477f
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-04-12 09:23:06 -0300

    update changelog and some improvements.

--- src/collective/cover/tiles/image.py
+++ src/collective/cover/tiles/image.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
-
+import time
+from Acquisition import aq_inner
 from zope.interface import implements
+from zope.component import queryMultiAdapter
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from plone.namedfile.field import NamedBlobImage as NamedImage
-from plone.namedfile.file import NamedBlobImage
+from plone.namedfile.file import NamedBlobImage as NamedImageFile
 
 from plone.tiles.interfaces import ITileDataManager
 
 from collective.cover import _
 from collective.cover.tiles.base import IPersistentCoverTile
 from collective.cover.tiles.base import PersistentCoverTile
+from collective.cover.tiles.base import AnnotationStorage
+from plone.scale.storage import AnnotationStorage as BaseAnnotationStorage
 
 
 class IImageTile(IPersistentCoverTile):
@@ -37,9 +41,21 @@ class ImageTile(PersistentCoverTile):
         # check permissions
         super(ImageTile, self).populate_with_object(obj)
 
-        data_mgr = ITileDataManager(self)
+        data = {}
+        obj = aq_inner(obj)
+        scales = queryMultiAdapter((obj, self.request), name="images")
+        if scales and scales.scale('image'):
+            data['image'] = NamedImageFile(str(scales.scale('image').data))
 
-        data_mgr.set({'image': NamedBlobImage(obj.getImage().data)})
+        data_mgr = ITileDataManager(self)
+        data_mgr.set(data)
+        tile_storage = AnnotationStorage(self)
+        obj_storage = BaseAnnotationStorage(obj)
+        for k, v in obj_storage.items():
+            tile_storage.storage[k] = v
+            tile_storage.storage[k]['modified'] = '%f' % time.time()
+            scale_data = obj_storage.storage[k]['data'].open().read()
+            tile_storage.storage[k]['data'] = NamedImageFile(str(scale_data))
 
     def accepted_ct(self):
         """ Return a list of content types accepted by the tile.
