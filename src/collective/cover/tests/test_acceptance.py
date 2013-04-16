cfeafb8427aa3ca57817901f3f6b9c6ba206bf7a
hvelarde <hector.velarde@gmail.com>
2013-04-15 21:58:20 -0300

    fix functional tests

--- src/collective/cover/tests/test_acceptance.py
+++ src/collective/cover/tests/test_acceptance.py
@@ -1,46 +1,39 @@
-import unittest
-
-import robotsuite
-
-from plone.testing import layered
+# -*- coding: utf-8 -*-
 
 from collective.cover.testing import FUNCTIONAL_TESTING
+from plone.testing import layered
+
+import robotsuite
+import unittest
 
 import pkg_resources
 PLONE_VERSION = pkg_resources.require("Plone")[0].version
+
+tests = [
+    'test_basic_tile.txt',
+    'test_collection_tile.txt',
+    'test_contentchooser_search_tab.txt',
+    'test_contenttree_tab.txt',
+    'test_contenttree_tab_path.txt',
+    'test_cover.txt',
+    'test_embed_tile.txt',
+    'test_file_tile.txt',
+    'test_image_tile.txt',
+    'test_layout.txt',
+    'test_list_tile.txt',
+    'test_locked_cover.txt',
+]
+
+# FIXME: test randomly failing under Plone 4.3.x
+#        see https://github.com/collective/collective.cover/issues/155
+if '4.3' in PLONE_VERSION:
+    tests.remove('test_contentchooser_search_tab.txt',)
 
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
-        layered(robotsuite.RobotTestSuite("test_cover.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_layout.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_basic_tile.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_contenttree_tab.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_contenttree_tab_path.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_collection_tile.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_embed_tile.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_file_tile.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_image_tile.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_list_tile.txt"),
-                layer=FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_locked_cover.txt"),
-                layer=FUNCTIONAL_TESTING),
+        layered(robotsuite.RobotTestSuite(t), layer=FUNCTIONAL_TESTING)
+        for t in tests
     ])
-    # FIXME: test randomly failing under Plone 4.3.x
-    #        see https://github.com/collective/collective.cover/issues/155
-    if '4.3' not in PLONE_VERSION:
-        suite.addTests([
-            layered(robotsuite.RobotTestSuite("test_contentchooser_search_tab.txt"),
-                    layer=FUNCTIONAL_TESTING),
-        ])
     return suite
