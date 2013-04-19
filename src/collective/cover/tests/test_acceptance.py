09ee021a55e04d5cc061bbfa45a8be98380ba64a
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-04-19 15:57:58 -0300

    Test name was changed, from test_screenlet_search_tab.txt to test_contentchooser_search_tab.txt

--- src/collective/cover/tests/test_acceptance.py
+++ src/collective/cover/tests/test_acceptance.py
@@ -11,14 +11,6 @@ dirname = os.path.dirname(__file__)
 files = os.listdir(dirname)
 tests = [f for f in files if f.startswith('test_') and f.endswith('.txt')]
 
-# FIXME: test randomly failing under Plone 4.3.x
-#        see https://github.com/collective/collective.cover/issues/155
-import pkg_resources
-PLONE_VERSION = pkg_resources.require("Plone")[0].version
-
-if '4.3' in PLONE_VERSION:
-    tests.remove('test_contentchooser_search_tab.txt',)
-
 
 def test_suite():
     suite = unittest.TestSuite()
