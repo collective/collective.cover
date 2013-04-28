011ee5d755456e7e3ee53f444fd83f1a7384831e
hvelarde <hector.velarde@gmail.com>
2013-04-28 17:41:51 -0300

    follow style guide

--- src/collective/cover/config.py
+++ src/collective/cover/config.py
@@ -2,10 +2,13 @@
 
 PROJECTNAME = 'collective.cover'
 
-DEFAULT_SEARCHABLE_CONTENT_TYPES = ['Collection',
-                                    'Document',
-                                    'File',
-                                    'Form Folder',
-                                    'Image',
-                                    'Link',
-                                    'News Item']
+# by default, all standard content types will be searchable
+DEFAULT_SEARCHABLE_CONTENT_TYPES = [
+    'Collection',
+    'Document',
+    'File',
+    'Form Folder',
+    'Image',
+    'Link',
+    'News Item'
+]
