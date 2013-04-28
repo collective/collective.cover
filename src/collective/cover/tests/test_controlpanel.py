0b0d0f4fc09f49fdacb220de2d217e9691acf075
hvelarde <hector.velarde@gmail.com>
2013-04-28 20:06:47 -0300

    refactor control panel tests

--- src/collective/cover/tests/test_controlpanel.py
+++ src/collective/cover/tests/test_controlpanel.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import unittest2 as unittest
-
-from zope.component import getMultiAdapter
-from zope.component import getUtility
-
-from plone.app.testing import TEST_USER_ID
-from plone.app.testing import logout
-from plone.app.testing import setRoles
-from plone.registry.interfaces import IRegistry
-
+from collective.cover.config import DEFAULT_AVAILABLE_TILES
 from collective.cover.config import DEFAULT_SEARCHABLE_CONTENT_TYPES
 from collective.cover.config import PROJECTNAME
 from collective.cover.controlpanel import ICoverSettings
 from collective.cover.testing import INTEGRATION_TESTING
+from plone.app.testing import logout
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.registry.interfaces import IRegistry
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+
+import unittest
 
 
 class ControlPanelTestCase(unittest.TestCase):
@@ -26,31 +25,28 @@ class ControlPanelTestCase(unittest.TestCase):
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
 
     def test_controlpanel_has_view(self):
-        view = getMultiAdapter((self.portal, self.portal.REQUEST),
-                               name='cover-settings')
+        view = getMultiAdapter(
+            (self.portal, self.portal.REQUEST), name='cover-settings')
         view = view.__of__(self.portal)
         self.assertTrue(view())
 
     def test_controlpanel_view_is_protected(self):
         from AccessControl import Unauthorized
         logout()
-        self.assertRaises(Unauthorized,
-                          self.portal.restrictedTraverse,
-                          '@@cover-settings')
+        self.assertRaises(
+            Unauthorized, self.portal.restrictedTraverse, '@@cover-settings')
 
     def test_controlpanel_installed(self):
         actions = [a.getAction(self)['id']
                    for a in self.controlpanel.listActions()]
-        self.assertTrue('cover' in actions,
-                        'control panel was not installed')
+        self.assertTrue('cover' in actions, 'control panel not installed')
 
     def test_controlpanel_removed_on_uninstall(self):
         qi = self.portal['portal_quickinstaller']
         qi.uninstallProducts(products=[PROJECTNAME])
         actions = [a.getAction(self)['id']
                    for a in self.controlpanel.listActions()]
-        self.assertTrue('cover' not in actions,
-                        'control panel was not removed')
+        self.assertTrue('cover' not in actions, 'control panel not removed')
 
 
 class RegistryTestCase(unittest.TestCase):
@@ -61,7 +57,6 @@ class RegistryTestCase(unittest.TestCase):
         self.portal = self.layer['portal']
         self.registry = getUtility(IRegistry)
         self.settings = self.registry.forInterface(ICoverSettings)
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
 
     def test_sections_record_in_registry(self):
         self.assertTrue(hasattr(self.settings, 'layouts'))
@@ -72,16 +67,16 @@ class RegistryTestCase(unittest.TestCase):
         self.assertListEqual(self.settings.searchable_content_types,
                              DEFAULT_SEARCHABLE_CONTENT_TYPES)
 
-    def get_record(self, record):
-        """ Helper function; it raises KeyError if the record is not in the
-        registry.
-        """
-        prefix = 'collective.cover.controlpanel.ICoverSettings.'
-        return self.registry[prefix + record]
-
     def test_records_removed_on_uninstall(self):
-        # XXX: I haven't found a better way to test this; anyone?
         qi = self.portal['portal_quickinstaller']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
         qi.uninstallProducts(products=[PROJECTNAME])
-        self.assertRaises(KeyError, self.get_record, 'layouts')
-        self.assertRaises(KeyError, self.get_record, 'searchable_content_types')
+
+        BASE_REGISTRY = 'collective.cover.controlpanel.ICoverSettings.%s'
+        records = [
+            BASE_REGISTRY % 'layouts',
+            BASE_REGISTRY % 'searchable_content_types',
+        ]
+
+        for r in records:
+            self.assertNotIn(r, self.registry)
