9f269127363da9811cf336062a025e4d9cf78cc7
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-05-10 17:05:31 -0300

    missing files for the cssclass widget...

---
+++ src/collective/cover/tiles/configuration_widgets/cssclasswidget.py
@@ -0,0 +1,25 @@
+import zope.interface
+import zope.schema
+from z3c.form.widget import FieldWidget
+from z3c.form.browser.select import SelectWidget
+from z3c.form import interfaces
+from interfaces import ICSSClassWidget
+
+
+@zope.interface.implementer_only(ICSSClassWidget)
+class CSSClassWidget(SelectWidget):
+    """Select widget implementation."""
+
+
+@zope.component.adapter(zope.schema.interfaces.IChoice,
+                        zope.interface.Interface,
+                        interfaces.IFormLayer)
+@zope.interface.implementer(interfaces.IFieldWidget)
+def CSSClassFieldWidget(field, source, request=None):
+    """IFieldWidget factory for SelectWidget."""
+    # BBB: emulate our pre-2.0 signature (field, request)
+    if request is None:
+        real_request = source
+    else:
+        real_request = request
+    return FieldWidget(field, CSSClassWidget(real_request))
