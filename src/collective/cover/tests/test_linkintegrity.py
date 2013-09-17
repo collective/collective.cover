c876442138de9739790df2e16a7c15e30c4b3f97
Juan Pablo Giménez <jpg@rosario.com>
2013-09-17 00:03:58 -0300

    Link integrity support, close #35

--- src/collective/cover/tests/test_linkintegrity.py
+++ src/collective/cover/tests/test_linkintegrity.py
@@ -1,32 +1,24 @@
+# -*- coding: utf-8 -*-
 # setup tests with all doctests found in docs/
 
-import pkg_resources
+import unittest2 as unittest
 from zope.interface import Interface
-from plone.app.linkintegrity.parser import extractLinks
 from Testing.ZopeTestCase import FunctionalDocFileSuite
 from Products.PloneTestCase import PloneTestCase
-from unittest import TestSuite, TestCase, makeSuite
+from unittest import TestSuite
 from os.path import join, split, abspath, dirname
 from os import walk
 from re import compile
 from sys import argv
 from collective.cover.tests import linkintegrity_docs as docs
-from plone.app.linkintegrity.tests import layer, utils
+from collective.cover.testing import LINKINTEGRITY_FUNCTIONAL_TESTING
+from plone.app.textfield import RichText
+from plone.app.testing.interfaces import (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+from plone.testing.z2 import Browser
 
 
-try:
-    pkg_resources.get_distribution('plone.app.referenceablebehavior')
-except pkg_resources.DistributionNotFound:
-    HAS_DEXTERITY = False
-    pass
-else:
-    HAS_DEXTERITY = True
-    from plone.dexterity.fti import DexterityFTI
-    from plone.app.textfield import RichText
-#    from plone.app.textfield.value import RichTextValue
-
-    class IMyDexterityItem(Interface):
-        text = RichText(title=u'Text')
+class IMyDexterityItem(Interface):
+    text = RichText(title=u'Text')
 
 
 PloneTestCase.setupPloneSite()
@@ -40,41 +32,23 @@ OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
                doctest.NORMALIZE_WHITESPACE)
 
 
-class LinkIntegrityFunctionalTestCase(PloneTestCase.FunctionalTestCase):
+class LinkIntegrityFunctionalTestCase(unittest.TestCase):
 
-    layer = layer.integrity
+    layer = LINKINTEGRITY_FUNCTIONAL_TESTING
 
-    def afterSetUp(self):
-        """ create some sample content to test with """
+    def setUp(self):
         # HTTPRequest's 'set' function is set to it's original implementation
         # at the start of each new test, since otherwise the below monkey
         # patch will apply to all remaining tests (and break them);  see
         # comment below in 'disableEventCountHelper'
         HTTPRequest.set = set_orig
-        self.setRoles(('Manager',))
-        fti = DexterityFTI('My Dexterity Item')
-        self.portal.portal_types._setObject('My Dexterity Item', fti)
-        fti.klass = 'plone.dexterity.content.Item'
-        fti.schema = 'plone.app.linkintegrity.tests.test_dexterity.IMyDexterityItem'
-        fti.behaviors = ('plone.app.referenceablebehavior.referenceable.IReferenceable',)
-        fti = DexterityFTI('Non referenciable Dexterity Item')
-        self.portal.portal_types._setObject('Non referenciable Dexterity Item', fti)
-        fti.klass = 'plone.dexterity.content.Item'
-        fti.schema = 'plone.app.linkintegrity.tests.test_dexterity.IMyDexterityItem'
-        self.portal.invokeFactory('My Dexterity Item', id='dexterity_item1',
-                                  title='Dexterity Item 1')
-        self.portal.invokeFactory('My Dexterity Item', id='dexterity_item2',
-                                  title='Dexterity Item 2')
-        self.portal.invokeFactory('Non referenciable Dexterity Item',
-                                  id='nonreferenciable_dexterity_item1',
-                                  title='Non referenciable Dexterity Item 1')
-        self.portal.invokeFactory('Non referenciable Dexterity Item',
-                                  id='nonreferenciable_dexterity_item2',
-                                  title='Non referenciable Dexterity Item 2')
-
-    def getBrowser(self, loggedIn=False):
-        """ instantiate and return a testbrowser for convenience """
-        return utils.getBrowser(loggedIn)
+        self.portal = self.layer['portal']
+        self.portal_url = self.portal.absolute_url()
+        self.browser = Browser(self.layer['app'])
+        self.tile = self.portal.cover1.restrictedTraverse(
+            '@@{0}/{1}'.format('collective.cover.richtext', 'test-richtext-tile'))
+        self.browser.handleErrors = True
+        self.browser.addHeader('Authorization', 'Basic {0}:{1}'.format(SITE_OWNER_NAME, SITE_OWNER_PASSWORD))
 
     def setStatusCode(self, key, value):
         from ZPublisher import HTTPResponse
@@ -98,13 +72,6 @@ class LinkIntegrityFunctionalTestCase(PloneTestCase.FunctionalTestCase):
         HTTPRequest.set = set
 
 
-class LinkIntegrityTestCase(TestCase):
-
-    def testHandleParserException(self):
-        self.assertEqual(extractLinks('<foo\'d>'), ())
-        self.assertEqual(extractLinks('<a href="http://foo.com">foo</a><bar\'d>'), ('http://foo.com',))
-
-
 # we check argv to enable testing of explicitely named doctests
 if '-t' in argv:
     pattern = compile('.*\.(txt|rst)$')
@@ -113,17 +80,14 @@ else:
 
 
 def test_suite():
-    suite = TestSuite([
-        makeSuite(LinkIntegrityTestCase),
-    ])
-    if HAS_DEXTERITY:
-        docs_dir = abspath(dirname(docs.__file__)) + '/'
-        for path, dirs, files in walk(docs_dir):
-            for name in files:
-                relative = join(path, name)[len(docs_dir):]
-                if not '.svn' in split(path) and pattern.search(name):
-                    suite.addTest(FunctionalDocFileSuite(relative,
-                                  optionflags=OPTIONFLAGS,
-                                  package=docs.__name__,
-                                  test_class=LinkIntegrityFunctionalTestCase))
+    suite = TestSuite()
+    docs_dir = abspath(dirname(docs.__file__)) + '/'
+    for path, dirs, files in walk(docs_dir):
+        for name in files:
+            relative = join(path, name)[len(docs_dir):]
+            if not '.svn' in split(path) and pattern.search(name):
+                suite.addTest(FunctionalDocFileSuite(relative,
+                              optionflags=OPTIONFLAGS,
+                              package=docs.__name__,
+                              test_class=LinkIntegrityFunctionalTestCase))
     return suite
