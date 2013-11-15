c6c514dd4be4ae94d9780e6b184aa396d3ec77f0
Maurits van Rees <maurits@vanrees.org>
2013-11-15 11:03:06 +0100

    Test layoutmanager_settings with two grids.

--- src/collective/cover/tests/test_cover.py
+++ src/collective/cover/tests/test_cover.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from collective.cover.content import ICover
+from collective.cover.controlpanel import ICoverSettings
 from collective.cover.testing import INTEGRATION_TESTING
+from collective.cover.testing import TWO_GRIDS_INTEGRATION_TESTING
 from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
 from plone.app.lockingbehavior.behaviors import ILocking
 from plone.app.referenceablebehavior.referenceable import IReferenceable
 from plone.app.stagingbehavior.interfaces import IStagingSupport
-from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
 from plone.dexterity.interfaces import IDexterityFTI
+from plone.registry.interfaces import IRegistry
 from plone.uuid.interfaces import IAttributeUUID
 from zope.component import createObject
+from zope.component import getUtility
 from zope.component import queryUtility
 
+import json
 import unittest
 
 
@@ -80,4 +85,37 @@ class CoverIntegrationTestCase(unittest.TestCase):
         self.assertFalse(self.layout_edit.can_export_layout())
         self.assertNotIn('<span>Export layout</span>', self.layout_edit())
 
+    def test_layoutmanager_settings(self):
+        settings = json.loads(self.layout_edit.layoutmanager_settings())
+        self.assertEqual(settings, {"ncolumns": 16})
+
     # TODO: add test for plone.app.relationfield.behavior.IRelatedItems
+
+
+class CoverTwoGridsIntegrationTestCase(unittest.TestCase):
+
+    layer = TWO_GRIDS_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        setRoles(self.portal, TEST_USER_ID, ['Member'])
+        self.folder = self.portal['test-folder']
+        self.folder.invokeFactory('collective.cover.content', 'c1',
+                                  template_layout='Layout A')
+        self.c1 = self.folder['c1']
+        self.layout_edit = self.c1.restrictedTraverse('layoutedit')
+
+    def test_layoutmanager_settings(self):
+        settings = json.loads(self.layout_edit.layoutmanager_settings())
+        self.assertEqual(settings, {"ncolumns": 16})
+
+        # Choose different grid.
+        registry = getUtility(IRegistry)
+        cover_settings = registry.forInterface(ICoverSettings)
+        cover_settings.grid_system = 'universe'
+
+        # The number of columns should be different now.
+        settings = json.loads(self.layout_edit.layoutmanager_settings())
+        self.assertEqual(settings, {"ncolumns": 42})
