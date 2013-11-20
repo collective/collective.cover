04a325c583a04761f646000e746b17884e6314f5
Maurits van Rees <maurits@vanrees.org>
2013-11-20 19:07:10 +0100

    Fix layoutedit tests.

--- src/collective/cover/tests/test_cover.py
+++ src/collective/cover/tests/test_cover.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from AccessControl import Unauthorized
 from collective.cover.content import ICover
 from collective.cover.controlpanel import ICoverSettings
 from collective.cover.testing import INTEGRATION_TESTING
@@ -34,7 +35,6 @@ class CoverIntegrationTestCase(unittest.TestCase):
         self.folder.invokeFactory('collective.cover.content', 'c1',
                                   template_layout='Layout A')
         self.c1 = self.folder['c1']
-        self.layout_edit = self.c1.restrictedTraverse('layoutedit')
 
     def test_adding(self):
         self.assertTrue(ICover.providedBy(self.c1))
@@ -77,16 +77,25 @@ class CoverIntegrationTestCase(unittest.TestCase):
     def test_export_permission(self):
         # layout export is visible for user with administrative rights
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.assertTrue(self.layout_edit.can_export_layout())
-        self.assertIn('<span>Export layout</span>', self.layout_edit())
+        layout_edit = self.c1.restrictedTraverse('layoutedit')
+        self.assertTrue(layout_edit.can_export_layout())
+        self.assertIn('<span>Export layout</span>', layout_edit())
 
-        # layout export is not visible for other users
+        # Accessing layoutedit as simple Member is not allowed.
         setRoles(self.portal, TEST_USER_ID, ['Member'])
-        self.assertFalse(self.layout_edit.can_export_layout())
-        self.assertNotIn('<span>Export layout</span>', self.layout_edit())
+        self.assertRaises(Unauthorized, self.c1.restrictedTraverse,
+                          'layoutedit')
+
+        # But we can cheat a bit here by reusing the layout_edit
+        # object that has been visited as a Manager above.  The
+        # layout export should still not be visible for this user.
+        self.assertFalse(layout_edit.can_export_layout())
+        self.assertNotIn('<span>Export layout</span>', layout_edit())
 
     def test_layoutmanager_settings(self):
-        settings = json.loads(self.layout_edit.layoutmanager_settings())
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        layout_edit = self.c1.restrictedTraverse('layoutedit')
+        settings = json.loads(layout_edit.layoutmanager_settings())
         self.assertEqual(settings, {'ncolumns': 16})
 
     # TODO: add test for plone.app.relationfield.behavior.IRelatedItems
@@ -105,10 +114,11 @@ class CoverMultipleGridsIntegrationTestCase(unittest.TestCase):
         self.folder.invokeFactory('collective.cover.content', 'c1',
                                   template_layout='Layout A')
         self.c1 = self.folder['c1']
-        self.layout_edit = self.c1.restrictedTraverse('layoutedit')
 
     def test_layoutmanager_settings(self):
-        settings = json.loads(self.layout_edit.layoutmanager_settings())
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        layout_edit = self.c1.restrictedTraverse('layoutedit')
+        settings = json.loads(layout_edit.layoutmanager_settings())
         self.assertEqual(settings, {'ncolumns': 16})
 
         # Choose different grid.
@@ -117,5 +127,5 @@ class CoverMultipleGridsIntegrationTestCase(unittest.TestCase):
         cover_settings.grid_system = 'bootstrap3'
 
         # The number of columns should be different now.
-        settings = json.loads(self.layout_edit.layoutmanager_settings())
+        settings = json.loads(layout_edit.layoutmanager_settings())
         self.assertEqual(settings, {'ncolumns': 12})
