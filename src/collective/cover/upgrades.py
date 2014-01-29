79e620a2215b53ec1a99f104c65406facd8a0dff
Maurits van Rees <maurits@vanrees.org>
2014-01-29 11:19:43 +0100

    Add upgrade step to cook css resources.

--- src/collective/cover/upgrades.py
+++ src/collective/cover/upgrades.py
@@ -88,3 +88,11 @@ def layout_edit_permission(context):
         return
     action = cover_type.getActionObject('object/layoutedit')
     action.permissions = (u'collective.cover: Can Edit Layout', )
+
+
+def cook_css_resources(context):
+    """Cook css resources.
+    """
+    css_tool = api.portal.get_tool('portal_css')
+    css_tool.cookResources()
+    logger.info('CSS resources were cooked')
