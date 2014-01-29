cc3f95bc209fb5a80748d97d7b02d547d0d6d851
Maurits van Rees <maurits@vanrees.org>
2014-01-29 11:20:50 +0100

    Prepare upgrade step to cook javascript resources.
    This is not needed at the moment, but surely in the future.

--- src/collective/cover/upgrades.py
+++ src/collective/cover/upgrades.py
@@ -96,3 +96,11 @@ def cook_css_resources(context):
     css_tool = api.portal.get_tool('portal_css')
     css_tool.cookResources()
     logger.info('CSS resources were cooked')
+
+
+def cook_javascript_resources(context):
+    """Cook javascript resources.
+    """
+    js_tool = api.portal.get_tool('portal_javascripts')
+    js_tool.cookResources()
+    logger.info('Javascript resources were cooked')
