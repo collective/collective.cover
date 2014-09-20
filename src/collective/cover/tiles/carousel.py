6ce67e2aa25ef7d51113e1801ae7bfb9674bb049
Manabu TERADA <terada@cmscom.jp>
2014-09-20 19:36:15 +0900

    Carousel extra space problem fix #414

--- src/collective/cover/tiles/carousel.py
+++ src/collective/cover/tiles/carousel.py
@@ -5,6 +5,7 @@ from collective.cover.interfaces import ITileEditForm
 from collective.cover.tiles.list import IListTile
 from collective.cover.tiles.list import ListTile
 from collective.cover.widgets.textlinessortable import TextLinesSortableFieldWidget
+from plone.memoize import view
 from plone.autoform import directives as form
 from plone.tiles.interfaces import ITileDataManager
 from plone.uuid.interfaces import IUUID
@@ -17,9 +18,9 @@ INIT_JS = """$(function() {{
     Galleria.loadTheme('++resource++collective.cover/galleria-theme/galleria.cover_theme.js');
     Galleria.run('#galleria-{0}');
 
-    var options = {{ height: 1 }};
+    var options = {{ height: {1}, showInfo: {2} }};
     if ($('body').hasClass('template-view')) {{
-        options.autoplay = {1};
+        options.autoplay = {3};
     }}
     Galleria.configure(options);
 }});
@@ -93,4 +94,35 @@ class CarouselTile(ListTile):
             # the <div> is there and has some items in it.
             return ''
 
-        return INIT_JS.format(self.id, str(self.autoplay()).lower())
+        return INIT_JS.format(self.id, str(self.get_image_ratio()),
+                              self.show_info(), str(self.autoplay()).lower())
+
+    def _get_description(self, item):
+        """Return the HTML code used for the description as configured on the tile.
+
+        :param item: [required]
+        :type item: content object
+        """
+        if self._field_is_visible('description'):
+            return item.Description()
+
+    @view.memoize
+    def get_description(self, item):
+        return self._get_description(item)
+
+    def get_image_ratio(self):
+        ratio = 0.0
+        items = self.results()
+        for item in items:
+            thumb = self.thumbnail(item)
+            ratio = max(ratio, float(thumb.height)/float(thumb.width))
+        if ratio:
+            return ratio
+        else:
+            return 1
+
+    def show_info(self):
+        if not self._field_is_visible('title') and not self._field_is_visible('description'):
+            return 'false'
+        else:
+            return 'true'
