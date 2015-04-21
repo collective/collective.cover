01e8640bcd28258db4018fdc909c2f954c957633
hvelarde <hector.velarde@gmail.com>
2015-04-20 22:52:55 -0300

    Add upgrade steps to cook the CSS and JS resources

--- src/collective/cover/tests/test_upgrades.py
+++ src/collective/cover/tests/test_upgrades.py
@@ -274,7 +274,7 @@ class Upgrade10to11TestCase(UpgradeTestCaseBase):
     def test_upgrade_to_11_registrations(self):
         version = self.setup.getLastVersionForProfile(self.profile_id)[0]
         self.assertTrue(int(version) >= int(self.to_version))
-        self.assertEqual(self._how_many_upgrades_to_do(), 3)
+        self.assertEqual(self._how_many_upgrades_to_do(), 5)
 
     def test_uuids_converted_to_dict(self):
         title = u'Revert PersistentMapping back to dict'
