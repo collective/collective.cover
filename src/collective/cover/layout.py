192c3f660b7be9e255a02eb82a410250fdfd1782
Fred van Dijk <fredvd@gmail.com>
2016-09-16 17:21:05 +0200

    Fix backwards compatibility for BaseGrid (#661)
    If you want to import BaseGrid, get it from the right location.

--- src/collective/cover/layout.py
+++ src/collective/cover/layout.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # TODO: this module must be removed in 1.4 release
-from collective.cover.grid import BaseGrid  # noqa
+from collective.cover.grids import BaseGrid  # noqa
 from zope import deprecation
 
 
