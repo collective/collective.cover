8ae5b72a52742458f5ff0cbb7d53c48687f409c6
Héctor Velarde <hvelarde@yahoo.com>
2017-04-04 18:09:56 -0300

    Update CI environment (#698)
    * Use Firefox 45.8.0esr instead of latest-esr
    
    * Use getIcon from plone_layout helper view
    
    This method was deprecated and has been removed from Plone 5.1.
    
    * Remove image traverses test from basic tile
    
    The test makes no sense as it was implemented and is failing with newer versions of Pillow.
    
    * Use Plone 4.3.11 for tests on 4.3 branch
    
    Refs: https://github.com/plone/plone.app.testing/issues/39
    
    * Use plone.api 1.6 on builds using Archetypes
    
    See: https://github.com/plone/plone.api/issues/364

--- src/collective/cover/tests/test_basic_tile.py
+++ src/collective/cover/tests/test_basic_tile.py
@@ -202,36 +202,6 @@ class BasicTileTestCase(TestTileMixin, unittest.TestCase):
         # there is no image...
         self.assertNotIn('test/@@images', rendered)
 
-    def test_image_traverser(self):
-        # XXX: probably this should be moved to a new test views module
-
-        def images_are_equal(str1, str2):
-            # XXX: refactor
-            from PIL import Image
-            from PIL import ImageChops
-            from StringIO import StringIO
-            im1 = StringIO()
-            im2 = StringIO()
-            im1.write(str1)
-            im1.seek(0)
-            im2.write(str2)
-            im2.seek(0)
-            return ImageChops.difference(Image.open(im1), Image.open(im2)).getbbox() is None
-
-        obj = self.portal['my-image']
-        self.tile.populate_with_object(obj)
-
-        scales = self.cover.restrictedTraverse(
-            '@@{0}/{1}/@@images'.format('collective.cover.basic', 'test'))
-        scale = scales.scale('image')
-
-        try:
-            image = obj.getImage()  # Archetypes
-        except AttributeError:
-            image = obj.image.data  # Dexterity
-
-        self.assertTrue(images_are_equal(image, scale.data.data))
-
     def test_basic_tile_image(self):
         obj = self.portal['my-news-item']
         self.tile.populate_with_object(obj)
