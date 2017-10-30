e3dd13af7d404138905b71ae36692d25afc1bf04
Héctor Velarde <hvelarde@yahoo.com>
2017-10-30 21:18:22 -0200

    Revert declaring cover_layout field in content type schema as readonly (#762)
    * Revert declaring cover_layout field in content type schema as readonly
    
    * Do no change anything under Plone 5
    
    Products.TinyMCE is not used in Plone 5 and we are not running RF tests neither.

--- src/collective/cover/testing.py
+++ src/collective/cover/testing.py
@@ -115,6 +115,16 @@ def generate_jpeg(width, height):
     return output
 
 
+# FIXME: workaround for https://github.com/plone/plone.app.testing/issues/39
+#        Products.TinyMCE is used only in Plone 4
+if not IS_PLONE_5:
+    autoform = ('plone.autoform', {'loadZCML': True})
+    tinymce = ('Products.TinyMCE', {'loadZCML': True})
+    products = list(PLONE_FIXTURE.products)
+    products.insert(products.index(tinymce), autoform)
+    PLONE_FIXTURE.products = tuple(products)
+
+
 class Fixture(PloneSandboxLayer):
 
     defaultBases = (PLONE_FIXTURE,)
