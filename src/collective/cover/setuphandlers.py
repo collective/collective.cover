50510dcc3c2294e7a294e167165a63613d04b8a2
hvelarde <hector.velarde@gmail.com>
2018-08-22 11:42:55 -0300

    Remove hard dependency on plone.app.relationfield

--- src/collective/cover/setuphandlers.py
+++ src/collective/cover/setuphandlers.py
@@ -2,6 +2,16 @@
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
+import pkg_resources
+
+
+try:
+    pkg_resources.get_distribution('plone.app.relationfield')
+except pkg_resources.DistributionNotFound:
+    HAS_RELATIONFIELD = False
+else:
+    HAS_RELATIONFIELD = True
+
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):  # pragma: no cover
@@ -12,3 +22,15 @@ class HiddenProfiles(object):  # pragma: no cover
             u'collective.cover:testfixture',
             u'collective.cover:uninstall',
         ]
+
+
+def add_relationfield_behavior():
+    """Add IRelatedItems behavior to FTI."""
+    from plone import api
+    fti = api.portal.get_tool('portal_types')['collective.cover.content']
+    fti.behaviors += ('plone.app.relationfield.behavior.IRelatedItems',)
+
+
+def run_after(portal_setup):
+    if HAS_RELATIONFIELD:
+        add_relationfield_behavior()
