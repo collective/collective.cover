fe34fed036c463c8d582c57472cfd97164a5e396
hvelarde <hector.velarde@gmail.com>
2018-08-23 17:28:24 -0300

    Avoid KeyError on installation if plone.app.relationfield is not available

--- src/collective/cover/setuphandlers.py
+++ src/collective/cover/setuphandlers.py
@@ -24,13 +24,15 @@ class HiddenProfiles(object):  # pragma: no cover
         ]
 
 
-def add_relationfield_behavior():
-    """Add IRelatedItems behavior to FTI."""
+def install_relationfield():
+    """Install plone.app.relationfield."""
     from plone import api
+    qi = api.portal.get_tool('portal_quickinstaller')
+    qi.installProduct('plone.app.relationfield')
     fti = api.portal.get_tool('portal_types')['collective.cover.content']
     fti.behaviors += ('plone.app.relationfield.behavior.IRelatedItems',)
 
 
 def run_after(portal_setup):
     if HAS_RELATIONFIELD:
-        add_relationfield_behavior()
+        install_relationfield()
