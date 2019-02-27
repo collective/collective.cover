9816884a410cd341981e97610059d07e655b1aa0
hvelarde <hector.velarde@gmail.com>
2019-02-26 20:50:05 -0600

    Classify package as Production/Stable

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='collective.cover',
       description=description,
       long_description=long_description,
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Environment :: Web Environment',
           'Framework :: Plone',
           'Framework :: Plone :: 4.3',
