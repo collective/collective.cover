303ff90206fce9dc228792c8cb7ddfa8caffde08
wesleybl <wesleybl@gmail.com>
2022-02-01 15:59:27 -0300

    Remove unnecessary code in handleSave.

--- src/collective/cover/tiles/edit.py
+++ src/collective/cover/tiles/edit.py
@@ -12,7 +12,6 @@ from plone.tiles.interfaces import ITileDataManager
 from plone.z3cform.interfaces import IDeferSecurityCheck
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from z3c.form import button
-from z3c.form import form
 from zope.component import getMultiAdapter
 from zope.interface import implementer
 from zope.publisher.interfaces.browser import IBrowserView
@@ -58,13 +57,8 @@ class CustomEditForm(DefaultEditForm):
         # fields that weren't sent with the form
         dataManager = ITileDataManager(tile)
         old_data = dataManager.get()
-
-        form.applyChanges(self, old_data, data)
-        for group in self.groups:
-            form.applyChanges(group, old_data, data)
-
-        current_data = self.getContent()
-        current_data.update(old_data)
+        for item in data:
+            old_data[item] = data[item]
         dataManager.set(old_data)
 
         api.portal.show_message(_(u"Tile saved"), self.request, type="info")
