fc48ae8987455c70886fdee114482c8cf2f110e9
wesleybl <wesleybl@gmail.com>
2023-03-29 17:09:49 -0300

    black

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     namespace_packages=["collective"],
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6.*',
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6.*",
     install_requires=[
         "AccessControl",
         "Acquisition",
